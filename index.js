41555b3a38564bbc5cda9d737aa2ded7851e3519
Roneesh <roneesh@gmail.com>
2016-01-31 13:07:05 -0600

    adding .gitignore and some basic tasks

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 #! /usr/bin/env node
 
-var exec = require('child_process').exec;
-var fs = require('fs');
+// Requires
+var exec = require('child_process').exec,
+	fs = require('fs');
 
-var user_arguments = process.argv.slice(2);
+// User arguments
+var cli_arguments = process.argv.slice(2),
+	user_command = cli_arguments[0],
+	user_arguments = cli_arguments.slice(3);
+
+switch(user_command) {
+	case 'add':
+		tasky_add(user_arguments);
+		break;
+	case 'list':
+		tasky_list(user_arguments);
+		break;
+	default:
+		tasky_list(user_arguments);
+}
+
+// Tasky Implementation
+function tasky_add() {
+	console.log('add')
+};
+
+function tasky_list() {
+	console.log('list')
+};
