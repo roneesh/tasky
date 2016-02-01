a42b11fa6fac27556c566ff1b01a4aca29cac98a
Roneesh <roneesh@gmail.com>
2016-02-01 01:39:10 -0600

    a different take for taksy

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 #! /usr/bin/env node
 
-// Requires
+// Requires and globals
 var exec = require('child_process').exec,
 	fs = require('fs');
+
+var _DB_FILE_NAME = 'tasky.json',
+	working_id = 0,
+	base_taky_config = {
+		index: 1,
+		tasks: []
+	}
 
 // User arguments
 var cli_arguments = process.argv.slice(2),
 	user_command = cli_arguments[0],
-	user_arguments = cli_arguments.slice(3);
+	user_arguments = cli_arguments.slice(1);
 
 switch(user_command) {
+	case 'init':
+		tasky_init();
+		break;
 	case 'add':
 		tasky_add(user_arguments);
 		break;
@@ -20,9 +30,42 @@ switch(user_command) {
 		tasky_list(user_arguments);
 }
 
-// Tasky Implementation
-function tasky_add() {
-	console.log('add')
+// Tasky Implementation API - do core tasky tasks like manage index of tasks, append and read from files, etc
+var tasky = {
+    getIndex: function() {
+    	fs.readFile('.tasky_config.json', "utf8", function(err, data) {
+	        if (err) throw err;
+	        return JSON.parse(data).index;
+	    });
+    },
+    incrementIndex: function() {
+    	fs.readFile('.tasky_config.json', "utf8", function(err, data) {
+	        if (err) throw err;
+	        return JSON.parse(data).index +
+	    });
+    }
+};
+
+
+
+// Tasky command line tasks, compose from tasky implementation API
+function tasky_init() {
+	fs.writeFileSync('.tasky_config.json', JSON.stringify(base_taky_config));
+}
+
+function tasky_add(user_arguments) {
+	var current_index;
+	fs.readFile('.tasky_config.json', "utf8", function(err, data) {
+        if (err) throw err;
+        current_index = JSON.parse(data).index;
+    });
+
+	fs.writeFileSync(_DB_FILE_NAME, JSON.stringify({
+		id: current_index + 1,
+		task: user_arguments,
+		completed: false,
+		tags: []
+	}));
 };
 
 function tasky_list() {
